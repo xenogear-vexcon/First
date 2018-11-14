09c593c322bdd967ce2c08b24714d58904c71160
Xenogear <36476697+xenogear-vexcon@users.noreply.github.com>
2018-11-15 00:41:59 +0300

    lesson-1 renew

--- Lesson-1/1-perfect_weight.rb
+++ Lesson-1/1-perfect_weight.rb
@@ -1,7 +1,7 @@
 puts 'Введите, пожалуйста, ваше имя и рост, и я скажу вам ваш идеальный вес!'
-print 'Ваше имя:  '
+print 'Ваше имя: '
 name = gets.chomp.capitalize
-print 'И ваш рост в см:  '
+print 'И ваш рост в см: '
 height = gets.to_f
 perfect_weight = height - 110
 if height > 110
